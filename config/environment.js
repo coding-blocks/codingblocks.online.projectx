ce8b99045a849db9c8f45f93cc9a46e903220297
Abhishek Gupta <hi@itsabhishek.me>
2018-01-31 15:36:16 +0530

    initial commit

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'codingblocks-online',
+    podModulePrefix: 'codingblocks-online/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
