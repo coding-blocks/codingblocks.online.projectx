8bae2c3d473fa6691fd7d366587cc896776680e1
Abhishek Gupta <hi@itsabhishek.me>
2018-02-07 18:45:21 +0530

    example loading recommendedcourses in index route

---
+++ app/pods/index/controller.js
@@ -0,0 +1,26 @@
+import Controller from '@ember/controller';
+import { service } from 'ember-decorators/service';
+import { task } from 'ember-concurrency';
+import { alias } from 'ember-decorators/object/computed';
+import { readOnly } from 'ember-decorators/object'
+
+export default class IndexController extends Controller {
+    @service store
+    @readOnly
+    @alias('fetchRecommendedCoursesTask.lastSuccessful.value') recommendedCourses
+
+    constructor () {
+        super(...arguments)
+        this.get('fetchRecommendedCoursesTask').perform()
+    }
+
+    fetchRecommendedCoursesTask = task(function * () {
+        return yield this.get('store').query('course', {
+            custom: {
+                ext: 'url',
+                url: 'all'
+            },
+            recommended: true
+        })
+    })
+}
