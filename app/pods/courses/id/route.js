5b4efcb693836ee8ebbcdbc1824e97fb188c5117
Abhishek Gupta <hi@itsabhishek.me>
2018-04-30 23:48:21 +0530

    link to course.id page on index and courses.index

--- app/pods/courses/id/route.js
+++ app/pods/courses/id/route.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    activate () {
+      window.scrollTo(0,0);
+    },
     model (params) {
         return this.store.findRecord("course", params.courseId)
     },
