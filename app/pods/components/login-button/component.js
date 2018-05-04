a666e4ec7c887bf6eec0db414fb85af00364a016
Abhishek Gupta <hi@itsabhishek.me>
2018-05-04 21:04:03 +0530

    refactor login-button component and add side-bar

--- app/pods/components/login-button/component.js
+++ app/pods/components/login-button/component.js
@@ -1,6 +1,27 @@
-import Component from '@ember/component';
-import env from 'codingblocks-online/config/environment';
+import Component from "@ember/component";
+import env from "codingblocks-online/config/environment";
+import { service } from "ember-decorators/service";
+import { action } from "ember-decorators/object";
 
 export default class LoginButton extends Component {
-    loginUrl = `${env.oneauthURL}/oauth/authorize?response_type=code&client_id=${env.clientId}&redirect_uri=${env.publicUrl}`
+  @service api;
+  @service session;
+  @service currentUser;
+
+  tagName = 'span'
+  loginUrl = `${env.oneauthURL}/oauth/authorize?response_type=code&client_id=${
+    env.clientId
+  }&redirect_uri=${env.publicUrl}`;
+
+  @action
+  invalidateSession() {
+    this.get("api")
+      .request("/jwt/logout")
+      .then(() => this.get("session").invalidate());
+  }
+
+  @action
+  logIn () {
+    window.location.href = this.get('loginUrl')
+  }
 }
