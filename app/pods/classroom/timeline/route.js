54c9dd92cb28ffe4936ca98aef8fd2f11fb12206
Abhishek Gupta <hi@itsabhishek.me>
2018-05-05 17:34:30 +0530

    add: enroll in preview button

--- app/pods/classroom/timeline/route.js
+++ app/pods/classroom/timeline/route.js
@@ -1,21 +1,47 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
+import { isNone } from "@ember/utils";
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
-    model (params) {
-        return this.store.queryRecord('run-attempt', {
-            runId: params.runId
-        }).then(runAttempt => {
-            return this.store.findRecord('run-attempt', runAttempt.get('id'), {reload: true})
-        })
-    },
-    setupController (controller, model) {
-        controller.set("run", model.get("run"))
-        controller.set("runAttempt", model)
-       // controller.set("course", model.get("topRunAttempt.run.course"))
-    },
-    actions: {
-        reloadRoute () {
-            this.refresh()
-        },
+  api: service(),
+  activate () {
+    window.scrollTo(0, 0)
+  },
+  model(params, transition) {
+    return this.store
+      .queryRecord("run-attempt", {
+        runId: params.runId
+      })
+      .then(runAttempt => {
+        if (isNone(runAttempt)) {
+          transition.abort();
+
+          // try to enroll in preview
+          return this.get("api")
+            .request(`runs/${params.runId}/enroll`)
+            .then(response => {
+              transition.retry()
+            })
+            .catch(err => {
+              throw new Error("Cannot enroll you in preview")
+            });
+        } else {
+          return this.store.findRecord("run-attempt",
+            runAttempt.get("id"), {
+              reload: true
+            }
+          );
+        }
+      })
+  },
+  setupController(controller, model) {
+    controller.set("run", model.get("run"));
+    controller.set("runAttempt", model);
+    // controller.set("course", model.get("topRunAttempt.run.course"))
+  },
+  actions: {
+    reloadRoute() {
+      this.refresh();
     }
+  }
 });
