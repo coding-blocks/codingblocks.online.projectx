4f6e09dd42c69dee5423b2a7c4bccb662689c31d
Abhishek Gupta <hi@itsabhishek.me>
2018-05-05 19:31:39 +0530

    hide nav-bar insde player

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,11 +1,19 @@
 import Controller from '@ember/controller';
 import { computed } from 'ember-decorators/object';
+import { or, alias } from 'ember-decorators/object/computed';
 import { service } from 'ember-decorators/service';
 
 export default class  ApplicationController extends Controller {
   @service router
   @computed ('router.currentRouteName')
-  isHidden () {
+  isInsideAttemptRoute () {
     return ['attempt'].includes(this.get('router.currentRouteName').split('.')[0])
   }
+  @computed ('router.currentRouteName')
+  isInsidePlayer () {
+    return ['player'].includes(this.get('router.currentRouteName').split('.')[0])
+  }
+
+  @or('isInsidePlayer', 'isInsideAttemptRoute') hideNav
+  @alias('isInsideAttemptRoute') hideFooter
 }
