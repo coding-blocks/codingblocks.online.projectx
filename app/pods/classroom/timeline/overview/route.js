b3c1f916929991a60cbed6185f591010402f5a8e
Abhishek Gupta <hi@itsabhishek.me>
2018-05-12 14:19:38 +0530

    show only 5 announcements

--- app/pods/classroom/timeline/overview/route.js
+++ app/pods/classroom/timeline/overview/route.js
@@ -2,30 +2,24 @@ import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
 
 export default Route.extend({
-  queryParams: {
-    offset: {
-      refreshModel: true
-    },
-    limit: {
-      refreshModel: true
-    }
+  model (params) {
+    return hash({
+        announcement: this.store.query("announcement", {
+          filter: {
+            runId: this.paramsFor('classroom.timeline').runId,
+          },
+          page: {
+            offset: 0,
+            limit: 5
+          },
+          order: '-updatedAt'
+        }),
+        run: this.modelFor('classroom.timeline').get("run")
+    });
   },
-    model (params) {
-      return hash({
-          announcement: this.store.query("announcement", {
-            runId: params.runId,
-            reload: true,
-            page: {
-              offset: params.offset,
-              limit: params.limit
-            }
-          }),
-          run: this.modelFor('classroom.timeline').get("run")
-      });
-    },
-    setupController (controller, model) {
-        controller.set("run", model.run)
-        controller.set("course", model.run.get('course'))
-        controller.set("announcements", model.announcement)
-    }
+  setupController (controller, model) {
+      controller.set("run", model.run)
+      controller.set("course", model.run.get('course'))
+      controller.set("announcements", model.announcement)
+  }
 });
