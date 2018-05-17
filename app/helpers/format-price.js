083b09defda8539349c75e63aa5f5c36467a4cdb
Vinayak <v27u27@gmail.com>
2018-05-17 07:24:38 +0530

    added NaN check in price

--- app/helpers/format-price.js
+++ app/helpers/format-price.js
@@ -2,7 +2,7 @@ import { helper } from '@ember/component/helper';
 
 export function formatPrice(params/*, hash*/) {
 	let price = parseInt(params[0]);
-	if(price == 0){
+	if(isNaN(price) || price == 0){
   		return "Free";
   	}
   	else{ 
