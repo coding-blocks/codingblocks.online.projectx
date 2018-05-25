85fbac80f50bae0380e75f85bc5f1db02ec4aed0
witty123 <warunhasija@gmail.com>
2018-05-26 03:27:04 +0530

    fix sentry dsn

--- config/environment.js
+++ config/environment.js
@@ -8,7 +8,7 @@ module.exports = function(environment) {
     rootURL: '/',
     locationType: 'auto',
     sentry: {
-      dsn: 'http://4847af8c7e2d4de8b5eafcb01093ac68@sentry.cb.lk/20'
+      dsn: 'https://4847af8c7e2d4de8b5eafcb01093ac68@sentry.cb.lk/20'
     },
     'ember-simple-auth-token': {
       identificationField: 'code',
@@ -69,7 +69,7 @@ module.exports = function(environment) {
     ENV.clientId = 5633768694
     ENV.oneauthURL = 'https://account.codingblocks.com'
     ENV.hackApiHost = 'https://api.cb.lk'
-    ENV.sentry.dsn = 'http://28a62ac6a0194a8381f4a6df014fa5ed@sentry.cb.lk/21'
+    ENV.sentry.dsn = 'https://28a62ac6a0194a8381f4a6df014fa5ed@sentry.cb.lk/21'
     ENV.googleAnalytics = {
       webPropertyId: 'UA-83327907-9'
     }
@@ -81,7 +81,7 @@ module.exports = function(environment) {
     ENV.clientId = 5169892443
     ENV.oneauthURL = 'https://account.codingblocks.com'
     ENV.hackApiHost = 'https://api.codingblocks.xyz' 
-    ENV.sentry.dsn = 'http://a1e58068dc5c48edb9b313b1efbe22ec@sentry.cb.lk/19'
+    ENV.sentry.dsn = 'https://a1e58068dc5c48edb9b313b1efbe22ec@sentry.cb.lk/19'
   }
 
   ENV['ember-simple-auth-token'].tokenPropertyName = 'jwt'
