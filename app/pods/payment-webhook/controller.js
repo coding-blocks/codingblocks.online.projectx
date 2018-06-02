33455f0638089d403b31ae252b7928e010a0f8bf
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-06-02 19:09:43 +0530

    Remove oneauth from paramsd

--- app/pods/payment-webhook/controller.js
+++ app/pods/payment-webhook/controller.js
@@ -1,7 +1,7 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['oneauth_id', 'transaction_id', 'products_id'],
+  queryParams: ['transaction_id', 'products_id'],
   transaction_id: null,
   products_id: null
 
