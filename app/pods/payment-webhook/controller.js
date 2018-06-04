91a68592f0880636a1f2cfa804fcad4358faadb2
Varun Hasija <warunhasija@gmail.com>
2018-06-04 15:30:30 +0530

    Update controller.js

--- app/pods/payment-webhook/controller.js
+++ app/pods/payment-webhook/controller.js
@@ -1,8 +1,7 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['transaction_id', 'products_id'],
+  queryParams: ['transaction_id', 'product_ids'],
   transaction_id: null,
-  products_id: null
-
+  product_ids: null
 });
