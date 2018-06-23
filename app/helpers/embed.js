14d1e205326d6bb6d11dbf0ec41bf6c39cccb528
Dhroov Gupta <dhroovgupta7@gmail.com>
2018-06-23 18:02:55 +0530

    Added autoplay youtube

--- app/helpers/embed.js
+++ app/helpers/embed.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 export function getEmbeded(param) {
-  if(param[0])
-    return (param[0]).replace('watch?v=','embed/')
+  if(param[0]){
+    return (param[0]).replace('watch?v=','embed/').concat('?autoplay=1&rel=0')
+  }
   else
     return null
 }
