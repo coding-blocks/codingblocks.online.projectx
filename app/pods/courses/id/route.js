0e1f4948c2059614e92381ebfc9c644539bc34bc
Abhishek Gupta <hi@itsabhishek.me>
2018-07-02 17:15:37 +0530

    cleanup

--- app/pods/courses/id/route.js
+++ app/pods/courses/id/route.js
@@ -7,9 +7,6 @@ export default Route.extend({
     model (params) {
         return this.store.findRecord("course", params.courseId)
     },
-    serialize (model) {
-        return { courseId: model.get('slug') || model.id }
-    },
     setupController(controller, model) {
         this._super(...arguments)
         controller.set("course", model)
