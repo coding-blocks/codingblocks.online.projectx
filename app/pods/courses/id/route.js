bf2af829c89d160c06b317845c02fea37107988a
Abhishek Gupta <hi@itsabhishek.me>
2018-07-02 16:49:33 +0530

    allow slugs to be used as course identifier

--- app/pods/courses/id/route.js
+++ app/pods/courses/id/route.js
@@ -7,6 +7,9 @@ export default Route.extend({
     model (params) {
         return this.store.findRecord("course", params.courseId)
     },
+    serialize (model) {
+        return { courseId: model.get('slug') || model.id }
+    },
     setupController(controller, model) {
         this._super(...arguments)
         controller.set("course", model)
