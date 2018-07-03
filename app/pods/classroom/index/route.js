b08311c99829fe34f2008d6ac7d5aabc9a150c95
Abhishek Gupta <hi@itsabhishek.me>
2018-07-03 22:50:09 +0530

    avoid multiple network request overhead for /classroom

--- app/pods/classroom/index/route.js
+++ app/pods/classroom/index/route.js
@@ -3,7 +3,8 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model () {
         return this.store.query('run', {
-            enrolled: true
+            enrolled: true,
+            include: 'course,run_attempts'
         })
     },
     setupController(controller, model) {
