fd82e764b9b6b883c5eddc363ff378137cb506ca
fire-finch <firefinchdev@gmail.com>
2018-07-06 18:42:13 +0530

    Modify: h1 and h2 tags in course titles and description

--- getMotley.sh
+++ getMotley.sh
@@ -2,7 +2,8 @@ rm -rf motley_tmp
 mkdir -p motley_tmp
 cd motley_tmp
 wget https://github.com/coding-blocks/motley/archive/master.zip
-tar -xzf master.zip
+unzip master.zip
+# tar -xzf master.zip
 rm -rf ../app/styles/*
 cp -rf motley-master/sass/styles/* ../app/styles/
 cd ..
