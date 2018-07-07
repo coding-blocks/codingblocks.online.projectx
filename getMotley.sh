8e4f47d7754a9e8c2fbe063a5df9665a38d1709b
Varun Hasija <warunhasija@gmail.com>
2018-07-08 02:25:53 +0530

    Update getMotley.sh

--- getMotley.sh
+++ getMotley.sh
@@ -2,8 +2,7 @@ rm -rf motley_tmp
 mkdir -p motley_tmp
 cd motley_tmp
 wget https://github.com/coding-blocks/motley/archive/master.zip
-unzip master.zip
-# tar -xzf master.zip
+tar -xzf master.zip
 rm -rf ../app/styles/*
 cp -rf motley-master/sass/styles/* ../app/styles/
 cd ..
