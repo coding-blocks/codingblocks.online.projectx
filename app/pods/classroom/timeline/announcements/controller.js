b79096875c1857a82527474ea58ac9025819ddf6
Abhishek Gupta <hi@itsabhishek.me>
2018-07-09 17:01:34 +0530

    fix announcement user details

--- app/pods/classroom/timeline/announcements/controller.js
+++ app/pods/classroom/timeline/announcements/controller.js
@@ -7,9 +7,8 @@ export default Controller.extend({
   limit: 3,
   actions: {
     updatePage: function() {
-      this.set("limit",this.get("meta.pagination.nextOffset")+this.get('limit'));
+      this.set("limit", this.get("meta.pagination.nextOffset")+this.get('limit'));
       if(this.get("limit") >= this.get("meta.pagination.count")) {
-          console.log("HH")
           this.set('visible', false);
       }
     }
