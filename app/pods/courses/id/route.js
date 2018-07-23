44b6a75ff5ccfce323db91e881ac62db5f18d13a
Abhishek Gupta <hi@itsabhishek.me>
2018-07-23 16:53:03 +0530

    404 if course is not found

--- app/pods/courses/id/route.js
+++ app/pods/courses/id/route.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
+import DS from 'ember-data';
+
+const { NotFoundError } = DS; 
 
 export default Route.extend({
   headData: Ember.inject.service(),
@@ -6,7 +9,11 @@ export default Route.extend({
       window.scrollTo(0,0);
     },
     model (params) {
-        return this.store.findRecord("course", params.courseId)
+        return this.store.findRecord("course", params.courseId).catch(err => {
+          if (err instanceof NotFoundError) {
+            this.transitionTo('/404')
+          }
+        })
     },
     setupController(controller, model) {
         this._super(...arguments)
