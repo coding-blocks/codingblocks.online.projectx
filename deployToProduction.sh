5ab8a56d2937cd65b8bbd67043a46bfda77afdad
Abhishek Gupta <hi@itsabhishek.me>
2018-07-23 12:55:18 +0530

    update deployToProduction.sh

--- deployToProduction.sh
+++ deployToProduction.sh
@@ -6,7 +6,7 @@ chmod 600 key.pem
 
 scp -o StrictHostKeyChecking=no -i key.pem -r dist/* $USER@$SERVER:~/frontends/codingblocks.online.projectx/new/
 ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf ~/frontends/codingblocks.online.projectx/old/*;"
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cp -r ~/frontends/codingblocks.online.projectx/current/* ~/frontends/codingblocks.online.projectx/old/;"
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cp -rf ~/frontends/codingblocks.online.projectx/current/* ~/frontends/codingblocks.online.projectx/old/;"
 ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf ~/frontends/codingblocks.online.projectx/current/*;"
-ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cp -r ~/frontends/codingblocks.online.projectx/new/* ~/frontends/codingblocks.online.projectx/current/*;"
+ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "cp -rf ~/frontends/codingblocks.online.projectx/new/* ~/frontends/codingblocks.online.projectx/current/*;"
 ssh -o StrictHostKeyChecking=no -i key.pem $USER@$SERVER "rm -rf ~/frontends/codingblocks.online.projectx/new/*;"
