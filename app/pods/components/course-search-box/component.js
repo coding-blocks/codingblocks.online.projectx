4f37fc2c9b45d313f8a827e6b1f753bd84c703e3
fire-finch <firefinchdev@gmail.com>
2018-08-10 22:30:06 +0530

    Fix: Optimised and fixed search on content

--- app/pods/components/course-search-box/component.js
+++ app/pods/components/course-search-box/component.js
@@ -1,7 +1,3 @@
-// import Component from '@ember/component';
-//
-// export default Component.extend({
-// });
 import Component from "@ember/component";
 import { later } from "@ember/runloop";
 import { task, timeout } from "ember-concurrency";
@@ -21,84 +17,69 @@ export default class SearchBoxComponent extends Component {
 
   searchTask = task(function * () {
     yield timeout (1000)
-    const searchQuery = this.get('qs')
-    // return this.get('api').request('courses/search', {
-    //   data: {
-    //     searchQuery
-    //   }
-    // }).then(response => {
-    //   this.set('hideResultsBox', false)
-    //   return response
-    // })
-    var searchResult = []
+    const searchQuery = this.get('qs').trim().toLowerCase()
 
-    const sections = this.get('run.sections')
+    try {
+      const sections = this.get('run.sections')
 
-    let contentIds = sections
-      .map(section => section.get('contents'))
-      .reduce((acc, val) => [...acc, ...val.toArray()], [])
-      .filter(content => {
-        return true
-      })
-      .map((content) => {
-        return {
-          title: content.get('payload.name'),
-          sectionId: content.get("section.id"),
-          section: content.get('section.name'),
-          contentId: content.get("id"),
-          isDone: content.get("isDone"),
-          iconClass: content.get('iconClass')
+      let contents = sections
+        .map(section => section.get('contents'))
+        .reduce((acc, val) => [...acc, ...val.toArray()], [])
+        .filter(content => {
+          //return true
+          return (content.get('payload.name')
+            .toLowerCase()
+            .indexOf(searchQuery) > -1)
+        })
+        .map((content) => {
+          return {
+            title: content.get('payload.name'),
+            sectionId: content.get('section.id'),
+            section: content.get('section.name'),
+            contentId: content.get('id'),
+            isDone: content.get('isDone'),
+            iconClass: content.get('iconClass')
+          }
+        })
+
+      if (contents.length === 0) {
+        return [
+          {
+            title: "Nothing Found!"
+          }
+        ]
+      }
+
+      console.log(contents)
+      return contents
+
+    } catch (e) {
+      console.error(e)
+      return [
+        {
+          title: "Some Error!"
         }
-      })
-
-    console.log(contentIds);
-    //
-    // contents.map((section) => {
-    //   section.get('contents').forEach(_ => searchResult.push(_.get('payload')))
-    // })
-    //
-    // searchResult.map((content) => {
-    //   return {
-    //     title: content.get('name'),
-    //     section: content.get("section").get("name")
-    //   }
-    // })
-
-
-    // console.log(searchResult)
-    return contentIds
-
-    // const contents = this.get('store').peekAll('content')
-    // contents.filter(function (item) {
-    //   return (content.get("payload").get("name").toLowerCase().indexOf(searchQuery.toLowerCase()) >= 0)
-    // }).map((content) => {
-    //   searchResult.push({
-    //     title: content.get("payload").get("name"),
-    //     section: content.get("section").get("name")
-    //   })
-    // })
-    // return searchResult
-
+      ]
+    }
   }).restartable()
 
   @action
-  transitonToResult (course) {
-    if (course.userEnrollment && course.userEnrollment.courseId) {
-      return this.get('router').transitionTo("classroom.timeline",
-        course.userEnrollment.courseId,
-        course.userEnrollment.runId
-      )
-    } else {
-      return this.get('router').transitionTo("courses.id", course.id)
-    }
+  transitionToContent(contentId, sectionId) {
+    this.get('router').transitionTo('attempt.content', contentId, {
+      queryParams: {
+        s: sectionId
+      }
+    });
   }
-
   @action
   showResult () {
     this.set('hideResultsBox', false);
   }
   @action
   hideResult () {
-    // later(this, () => this.set('hideResultsBox', true), 100);
+    later(this, () => {
+      this.set('qs', '')
+      this.set('hideResultsBox', true)
+    }, 100);
   }
 }
