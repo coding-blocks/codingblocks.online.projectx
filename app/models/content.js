72e486d614493defa90940cf6976434d6fb98ae7
Abhishek Gupta <hi@itsabhishek.me>
2018-08-15 03:26:26 +0530

    chatty courses/:slug

--- app/models/content.js
+++ app/models/content.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   contentable: DS.attr(),
+  title: DS.attr(),
+  sectionContent: DS.attr(),
   course: DS.belongsTo('course'),
   qna: DS.belongsTo('qna'),
   lecture: DS.belongsTo('lecture'),
