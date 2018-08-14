e63bc63e8346e9682689e72f02c6df744a1f2500
Abhishek Gupta <hi@itsabhishek.me>
2018-08-14 17:22:53 +0530

    show expired run_attempts as expired

--- app/models/run-attempt.js
+++ app/models/run-attempt.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 export default DS.Model.extend({
   name: DS.attr(),
   premium: DS.attr(),
+  end: DS.attr(),
+  isExpired: computed('end', function () {
+    return this.get('end') < +new Date()/1000
+  }),
   run: DS.belongsTo('run'),
   user: DS.belongsTo('user'),
 })
