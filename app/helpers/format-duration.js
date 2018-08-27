9fa021bc1b515b52ac9dc44cdbb8bd814b9f99e3
fire-finch <firefinchdev@gmail.com>
2018-08-27 17:19:56 +0530

    Add: Section Length

--- app/helpers/format-duration.js
+++ app/helpers/format-duration.js
@@ -5,17 +5,24 @@ const fixedDigits = (val) => ('0' + +val).slice(-2)
 
 export function formatDuration(params) {
 	let duration = parseInt(params[0]);
+	let options = params[1];
 	if(isNaN(duration) || duration == 0){
 			return "--";
-		}
-		else{
-			let res = moment.duration(duration);
-      const hours = fixedDigits(Math.floor(res.asHours()))
-      const mins = fixedDigits(res.minutes())
-      const secs = fixedDigits(res.seconds())
+	} else {
+    let res = moment.duration(duration);
+    let hours = Math.floor(res.asHours())
+    let mins = res.minutes()
+    let secs = res.seconds()
 
+    if (options == 'humanize') {
+      return (hours ? `${hours} Hours `:'') + (mins ? `${mins} Minutes`:'')
+    } else {
+      hours = fixedDigits(hours)
+      mins = fixedDigits(mins)
+      secs = fixedDigits(secs)
       return `${hours}:${mins}:${secs}`;
-		}
+    }
+	}
 }
 
 export default helper(formatDuration);
