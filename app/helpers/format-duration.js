1235e9b92c09dcf959da0185f313149ff0b0f8ba
fire-finch <firefinchdev@gmail.com>
2018-08-27 16:30:18 +0530

    Updated format-duration helper

--- app/helpers/format-duration.js
+++ app/helpers/format-duration.js
@@ -1,17 +1,21 @@
 import { helper } from '@ember/component/helper';
 import moment from 'moment';
 
-export function formatPrice(params/*, hash*/) {
+const fixedDigits = (val) => ('0' + +val).slice(-2)
+
+export function formatDuration(params) {
 	let duration = parseInt(params[0]);
 	if(isNaN(duration) || duration == 0){
 			return "--";
 		}
 		else{
-			let res = moment.duration(duration)._data;
-      return (res.hours?`0${res.hours}`.slice(-2) + ':':'00:')
-        + (res.minutes?`0${res.minutes}`.slice(-2) + ':':'00:')
-        + (res.seconds?`0${res.seconds}`.slice(-2) :'00');
+			let res = moment.duration(duration);
+      const hours = fixedDigits(Math.floor(res.asHours()))
+      const mins = fixedDigits(res.minutes())
+      const secs = fixedDigits(res.seconds())
+
+      return `${hours}:${mins}:${secs}`;
 		}
 }
 
-export default helper(formatPrice);
+export default helper(formatDuration);
