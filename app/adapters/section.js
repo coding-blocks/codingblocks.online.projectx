155cc7ba91171db7c16fb3bba56772d770f7554f
Abhishek Gupta <hi@itsabhishek.me>
2018-08-29 17:16:08 +0530

    use the exclude qp for getting sections at /course/:slug

--- app/adapters/section.js
+++ app/adapters/section.js
@@ -3,6 +3,6 @@ import ApplicationAdapter from './application'
 export default ApplicationAdapter.extend({
     urlForFindRecord (id, modelName, snapshot) {
         let baseUrl = this.buildURL(modelName, id, snapshot)
-        return `${baseUrl}/?include=contents`
+        return `${baseUrl}/?include=contents&exclude=contents.*`
     }
 })
