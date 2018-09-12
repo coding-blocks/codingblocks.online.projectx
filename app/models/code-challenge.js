dbda4975694c9195a68e320bb02fd521d0f1b5bb
Anirudh Gupta <firefinchdev@gmail.com>
2018-09-12 19:47:45 +0530

    Code Challenge Model Changes

--- app/models/code-challenge.js
+++ app/models/code-challenge.js
@@ -4,12 +4,5 @@ import env from 'codingblocks-online/config/environment'
 
 export default DS.Model.extend({
     name: DS.attr(),
-    hbContestId: DS.attr(),
-    hbProblemId: DS.attr(),
-    hbUrl: computed ('hbContestId', 'hbProblemId', function () {
-    const hbContestId = this.get('hbContestId'),
-        hbProblemId = this.get('hbProblemId')
-
-    return `${env.hbBaseUrl}/contests/c/${hbContestId}/${hbProblemId}`
-    })
+    hbProblemId: DS.attr()
 })
