c9e757b6e747927014af26cbda9fcf670e77c35c
Anirudh Gupta <firefinchdev@gmail.com>
2018-10-06 12:54:41 +0530

    Add: Email in Instructor Model

--- app/models/instructor.js
+++ app/models/instructor.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
     name: DS.attr(),
     description: DS.attr(),
     photo: DS.attr(),
-    courses: DS.hasMany('course')
+    courses: DS.hasMany('course'),
+    email: DS.attr()
 })
