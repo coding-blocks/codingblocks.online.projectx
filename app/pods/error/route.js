668db54fd0901f0e89da3759c2cd7998ec2ec58d
Abhishek Gupta <hi@itsabhishek.me>
2018-10-11 17:00:35 +0530

    error message fix for dukaan

--- app/pods/error/route.js
+++ app/pods/error/route.js
@@ -5,7 +5,7 @@ export default Route.extend({
     PAYMENT_FAILED: 'Please retry the payment by going to <a href="https://dukaan.codingblocks.com/mycart" style="text-decoration: underline; color: blue">Your cart.</a>',
     USER_EMAIL_NOT_VERIFIED: `You need to verify your email before you can access any of our courses. You can do so on the <a href="https://account.codingblocks.com/" style="text-decoration: underline; color: blue">profile page here.</a><br> <p>If you have already verified your email and still seeing this, you need to logout, and log In again.</p>`,
     NO_CONTENT: `There is not content to display for this course yet. Please try again later, or ask your mentor/support about this.`,
-    DUKKAN_ERROR: `There was some error in communicating to dukaan.codingblocks.com. Please Contact Codingblocks Support`,
+    DUKKAN_ERROR: `You already have a product in your cart. Please complete your payment by going to <a href="https://dukaan.codingblocks.com/mycart" style="text-decoration: underline; color: blue">your cart</a>  or contact coding blocks support for any further help.`,
     NO_USER_MOBILE_NUMBER: `You need to add your mobile number before you can access any of our courses. You can do so on the <a href="https://account.codingblocks.com/" style="text-decoration: underline; color: blue">profile page here.</a><br> <p>If you have already added your mobile and still seeing this, you need to logout, and log In again.</p>`
   },
   queryParams: {
