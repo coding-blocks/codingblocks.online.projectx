abad1d3fd23f5587a27b4f22bbc5ba0de38d3def
Abhishek Gupta <hi@itsabhishek.me>
2018-12-21 11:43:57 +0530

    add org view for All courses as well

--- app/pods/courses/index/route.js
+++ app/pods/courses/index/route.js
@@ -1,33 +1,44 @@
 import Route from '@ember/routing/route';
+import { service } from 'ember-decorators/service'
 
-export default class CoursesRouter extends  Route {
-    constructor() {
-      super()
-      this.headData =  Ember.inject.service()
+export default class CoursesRouter extends Route {
+  @service currentUser
+
+  constructor() {
+    super()
+    this.headData = Ember.inject.service()
+  }
+
+  model(params) {
+    const organization = this.get('currentUser.user.organization')
+    const extraWhere = {}
+
+    if (organization) {
+      extraWhere.organization = organization
     }
 
-    model (params) {
-         return this.store.query ('course', {
-          include: 'runs',
-          sort: 'difficulty',
-          exclude: "ratings",
-          filter: {
-            unlisted: false
-          },
-          page:{
-            limit:params.limit,
-            offset:params.offset
-          }
-        })
-    }
+    return this.store.query('course', {
+      include: 'runs',
+      sort: 'difficulty',
+      exclude: "ratings",
+      filter: {
+        unlisted: false,
+        ...extraWhere
+      },
+      page: {
+        limit: params.limit,
+        offset: params.offset
+      }
+    })
+  }
 
-    setupController (controller, model) {
-      controller.set('courses', model.toArray())
-      controller.set("nextOffset", model.get('meta').pagination.nextOffset )
-      controller.set("count", model.get('meta').pagination.count )
+  setupController(controller, model) {
+    controller.set('courses', model.toArray())
+    controller.set("nextOffset", model.get('meta').pagination.nextOffset)
+    controller.set("count", model.get('meta').pagination.count)
 
-    }
-    afterModel(model) {
-      this.set('headData.title', 'Coding Blocks Online | All Courses')
-    }
+  }
+  afterModel(model) {
+    this.set('headData.title', 'Coding Blocks Online | All Courses')
+  }
 }
