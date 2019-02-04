62c770a03675030944a778023b689002d4b5ed9e
Abhishek Gupta <hi@itsabhishek.me>
2019-02-04 23:28:32 +0530

    bufix: timeline contents lock icon

--- app/pods/classroom/timeline/contents/route.js
+++ app/pods/classroom/timeline/contents/route.js
@@ -7,5 +7,6 @@ export default Route.extend({
     setupController (controller, model) {
         controller.set("run", model.get("run"))
         controller.set("sections", model.get("run.sections"))
+        this._super(...arguments)
     }
 });
