bb3a474963cc5eda58ad8397de0c7a1dab7a5822
Abhishek Gupta <hi@itsabhishek.me>
2019-02-06 14:25:12 +0530

    remove dead code

--- app/pods/classroom/timeline/index/route.js
+++ app/pods/classroom/timeline/index/route.js
@@ -1,14 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    beforeModel () {
+    activate () {
       this.transitionTo('classroom.timeline.overview')  
-    },
-    model () {
-       return this.modelFor('classroom.timeline')
-    },
-    setupController(controller, model) {
-        controller.set("course", model.get("course"))
-        controller.set("run", model)
     }
 });
