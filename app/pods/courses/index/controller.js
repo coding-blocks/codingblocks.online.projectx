d8da52556cad06fac902b496fd4ad45c9fc8c62f
Abhishek Gupta <hi@itsabhishek.me>
2019-03-04 17:44:08 +0530

    show loading button when loading all courses

--- app/pods/courses/index/controller.js
+++ app/pods/courses/index/controller.js
@@ -33,7 +33,7 @@ export default Ember.Controller.extend({
   }),
 
   actions:{
-    loadMore: function() {
+    loadMore () {
       this.set('limit', Math.min(this.get('limit') + 9, this.get('count')))
       this.get('taskMoreCourses').perform()
     }
