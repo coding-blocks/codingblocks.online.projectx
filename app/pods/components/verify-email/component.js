6f45309107cf7469c5ffb4d7b51000c350343832
Jatin Katyal <jatin.katyal13@gmail.com>
2019-03-17 02:21:02 +0530

    add retry button

--- app/pods/components/verify-email/component.js
+++ app/pods/components/verify-email/component.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
+import env from "codingblocks-online/config/environment";
+import { action } from "@ember-decorators/object";
+import { inject as service } from '@ember-decorators/service';
 
 export default class VerifyEmailComponent extends Component {
+  @service session
+
+  loginUrl = `${env.oneauthURL}/oauth/authorize?response_type=code&client_id=${
+    env.clientId
+  }&redirect_uri=${window.location.href}`;
+
+  @action
+  retryLogin() {
+    debugger
+    this.session.invalidate()
+      .then(() => {
+        localStorage.setItem('redirectionPath', this.get('router.currentURL'))
+        window.location.href = this.get('loginUrl')
+      })
+  }
 }
