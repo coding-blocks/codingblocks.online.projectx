2e5a4a856fd55e2a8edcef4082686dc50f70903d
Jatin Katyal <jatin.katyal13@gmail.com>
2019-03-20 12:32:25 +0530

    proper routing

--- app/pods/components/verify-email/component.js
+++ app/pods/components/verify-email/component.js
@@ -1,22 +1,17 @@
 import Component from '@ember/component';
-import env from "codingblocks-online/config/environment";
 import { action } from "@ember-decorators/object";
 import { inject as service } from '@ember-decorators/service';
 
 export default class VerifyEmailComponent extends Component {
   @service session
-
-  loginUrl = `${env.oneauthURL}/oauth/authorize?response_type=code&client_id=${
-    env.clientId
-  }&redirect_uri=${window.location.href}`;
+  @service router
 
   @action
   retryLogin() {
-    debugger
     this.session.invalidate()
       .then(() => {
         localStorage.setItem('redirectionPath', this.get('router.currentURL'))
-        window.location.href = this.get('loginUrl')
+        this.router.transitionTo('login')
       })
   }
 }
