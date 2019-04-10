147348f3044d4680c4e036d4acb67c952f746048
Jatin Katyal <jatin.katyal13@gmail.com>
2019-04-10 17:14:19 +0530

    pagination in all courses

--- app/pods/components/my-courses/component.js
+++ app/pods/components/my-courses/component.js
@@ -1,27 +1,37 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember-decorators/service';
 import { restartableTask } from 'ember-concurrency-decorators';
-import { alias } from '@ember-decorators/object/computed';
-import { readOnly } from '@ember-decorators/object/computed';
-
 
 export default class MyCoursesTaskComponent extends Component {
   @service store;
   @service api;
-  @readOnly("fetchMyCoursesTask.lastSuccessful.value") runs
+  runs = null
+
+  LIMIT = 8
+  OFFSET = 0
 
   didReceiveAttrs () {
     this._super(...arguments)
     this.get('fetchMyCoursesTask').perform()
-}
+  }
 
   @restartableTask
   *fetchMyCoursesTask () {
-    return yield this.get("store").query("run", {
+    const runs = yield this.get("store").query("run", {
       include: "course,run_attempts",
-      enrolled: true
+      enrolled: true,
+      page: {
+        offset: this.OFFSET,
+        limit: this.LIMIT
+      }
     });
-  }
-
+    this.set('OFFSET', this.OFFSET + this.LIMIT)
     
+    if (!this.runs) {
+      this.set('runs', runs.toArray())
+    } else {
+      this.get('runs').pushObjects(runs.toArray())
+    }
+    return runs
+  }
 }
