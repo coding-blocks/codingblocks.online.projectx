0c340a38833bf01e4c1fee223771a9f6144f4231
ananay <i@ananayarora.com>
2019-05-23 17:45:42 +0530

    Added pagination to My Courses page

---
+++ app/pods/classroom/index/controller.js
@@ -0,0 +1,43 @@
+import Controller from '@ember/controller';
+import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+import { alias }  from '@ember/object/computed';
+
+export default Controller.extend({
+    queryParams: ['limit', 'offset', 'org'],
+    limit: 8,
+    offset: 0,
+    currentUser: service(),
+    organization: alias('currentUser.organization'),
+    taskMoreCourses: task(function * () {
+        const extraWhere = {}
+        const organization = this.organization || this.org
+    
+        if (organization) {
+            extraWhere.organization = organization
+        }
+
+        const nextCourses = yield this.store.query ('course', {
+            include: 'instructors,runs',
+            sort: 'difficulty',
+            exclude: 'ratings,instructors.*',
+            filter: {
+                unlisted: false,
+                ...extraWhere
+            },
+            page:{
+                limit: this.limit,
+                offset: this.offset
+            }
+        })
+        this.courses.addObjects(nextCourses)
+    }),
+    
+    actions:{
+        loadMore () {
+            this.set('limit', Math.min(this.limit + 8, this.count))
+            this.taskMoreCourses.perform()
+        }
+    }
+    
+});
--- app/pods/components/my-courses/component.js
+++ app/pods/components/my-courses/component.js
@@ -3,25 +3,57 @@ import { inject as service } from '@ember-decorators/service';
 import { restartableTask } from 'ember-concurrency-decorators';
 import { alias } from '@ember-decorators/object/computed';
 import { readOnly } from '@ember-decorators/object/computed';
-
+import { action, computed } from '@ember-decorators/object'
 
 export default class MyCoursesTaskComponent extends Component {
-  @service store;
-  @service api;
-  @readOnly("fetchMyCoursesTask.lastSuccessful.value") runs
+  	@service store;
+  	@service api;
 
-  didReceiveAttrs () {
-    this._super(...arguments)
-    this.get('fetchMyCoursesTask').perform()
-}
+  	didReceiveAttrs () {
+		this._super(...arguments)
+		this.limit = 8;
+		this.offset = 8;
+		this.runs = [];
+		this.count = 0;
+		this.get('fetchMyCoursesTask').perform()
+	}
 
-  @restartableTask
-  *fetchMyCoursesTask () {
-    return yield this.get("store").query("run", {
-      include: "course,run_attempts",
-      enrolled: true
-    });
-  }
+	/**
+	 * Limit and offset sum computed property
+	 */
 
-    
+	@computed ("limit", "offset")
+	get limitandoffset () {
+		console.log(this.get('limit') + this.get('offset'));
+		return (this.get('limit') + this.get('offset'));
+	}
+
+	/**
+	 * Fetch courses on the 'My Courses' page
+	 */
+
+  	@restartableTask
+  	*fetchMyCoursesTask () {
+    	const results = yield this.get("store").query("run", {
+    	include: "course,run_attempts",
+			enrolled: true,
+			page:{
+				limit: this.limit,
+				offset: this.offset
+			}
+		});
+		this.set('count', results.meta.pagination.count);
+		this.runs.addObjects(results);
+	}
+
+	/**
+	 * 'Load More' Button Action Handler for 'My Courses'
+	 */
+	
+	@action
+	loadMore () {
+		this.set('offset', this.offset + 8);
+		this.fetchMyCoursesTask.perform();
+	}
+	 
 }
