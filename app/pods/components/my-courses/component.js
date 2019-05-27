7c183534afd84cd8c5a690bff96a642d4a8b1a7a
Ananay Arora <ananay@users.noreply.github.com>
2019-05-27 12:27:07 +0530

    Added Infinite Scroll to My Courses page (#415)
    * Added pagination to My Courses page
    
    * Added Infinite Scroll
    
    * Removed unused imports
    
    * Converted tabs to spaces of size 2
    
    * Removed extra unnecessary file
    
    * Removed load more button and changed to spaces
    
    * Changed to LoadingCardClassroom from three-dots

--- app/pods/components/my-courses/component.js
+++ app/pods/components/my-courses/component.js
@@ -1,27 +1,71 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember-decorators/service';
 import { restartableTask } from 'ember-concurrency-decorators';
-import { alias } from '@ember-decorators/object/computed';
-import { readOnly } from '@ember-decorators/object/computed';
-
+import { action, computed } from '@ember-decorators/object'
 
 export default class MyCoursesTaskComponent extends Component {
   @service store;
   @service api;
-  @readOnly("fetchMyCoursesTask.lastSuccessful.value") runs
-
-  didReceiveAttrs () {
-    this._super(...arguments)
+  
+  limit = 8
+  offset = 8
+  runs = []
+  count = 0
+  
+  constructor () {
+    super(...arguments)
+    this.infiniteScroll = () => {
+      if (document.getElementById('load-more') != null) {
+        if (window.pageYOffset >= (document.getElementById('load-more').offsetTop - window.innerHeight)) {
+          this.loadMore();
+        }
+      }
+    }
     this.get('fetchMyCoursesTask').perform()
-}
-
+  }
+  
+  didInsertElement () {
+    window.addEventListener('scroll', this.infiniteScroll);
+  }
+  
+  willDestroyElement () {
+    window.removeEventListener('scroll', this.infiniteScroll);
+  }
+  
+  /**
+  * Limit and offset sum computed property
+  */
+  
+  @computed ("limit", "offset")
+  get limitandoffset () {
+    return (this.get('limit') + this.get('offset'));
+  }
+  
+  /**
+  * Fetch courses on the 'My Courses' page
+  */
+  
   @restartableTask
   *fetchMyCoursesTask () {
-    return yield this.get("store").query("run", {
+    const results = yield this.get("store").query("run", {
       include: "course,run_attempts",
-      enrolled: true
+      enrolled: true,
+      page: {
+        limit: this.limit,
+        offset: this.offset
+      }
     });
+    this.set('count', results.meta.pagination.count);
+    this.runs.addObjects(results);
   }
+  
+  /**
+  * 'Load More' Button Action Handler for 'My Courses'
+  */
 
-    
+  @action
+  loadMore () {
+    this.set('offset', this.offset + 8);
+    this.fetchMyCoursesTask.perform();
+  }
 }
