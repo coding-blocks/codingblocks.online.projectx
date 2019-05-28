1f364ec1ce7c5541255ec7f07643ebd8582b7f94
ananay <i@ananayarora.com>
2019-05-28 22:37:54 +0530

    Humanized dates and capitalized names

--- app/pods/components/player-csv-leaderboard/component.js
+++ app/pods/components/player-csv-leaderboard/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember-decorators/service';
 import { restartableTask } from 'ember-concurrency-decorators';
+const moment = require('moment');
 
 export default class PlayerCsvLeaderboardComponent extends Component {
 
@@ -10,12 +11,26 @@ export default class PlayerCsvLeaderboardComponent extends Component {
     this.fetchLeaderboardTask.perform();
   }
 
+  /**
+   * Fetches the leaderboard data for the 
+   * CSV submission and humanizes the timestamp
+   */
+
   @restartableTask
   *fetchLeaderboardTask() {
     let csvId = this.get('csvId');
+
+    // Fetch the leaderboard data from the server
     this.api.request(`/csvs/${csvId}/leaderboard`, {
       method: 'GET'
     }).then(response => {
+      // Humanize the 'createdAt' time
+      for (let i = 0; i < response.length; i++) {
+        response[i].firstname = response[i].firstname.charAt(0).toUpperCase() + response[i].firstname.slice(1).toLowerCase();
+        response[i].lastname = response[i].lastname.charAt(0).toUpperCase() + response[i].lastname.slice(1).toLowerCase();
+        response[i].name = response[i].firstname + " " + response[i].lastname;
+        response[i].createdAt = moment.duration(moment(response[i].createdAt).diff(moment.now())).humanize() + " ago";
+      }
       this.set('lb', response);
     });
   }
