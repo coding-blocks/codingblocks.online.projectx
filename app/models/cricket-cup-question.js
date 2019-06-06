0d0d12caa000a32d1dfa723ec8ed54d5b7767cef
Jatin Katyal <jatin.katyal13@gmail.com>
2019-06-07 02:11:05 +0530

    correct incorrect

--- app/models/cricket-cup-question.js
+++ app/models/cricket-cup-question.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   title: DS.attr(),
   description: DS.attr(),
   score: DS.attr(),
-  correctChoiceId: DS.attr(),
+  correctChoiceId: DS.attr('string'),
   cricketCupChoices: DS.hasMany('cricketCupChoice'),
   cricketCupMatch: DS.belongsTo('cricketCupMatch')
 })
