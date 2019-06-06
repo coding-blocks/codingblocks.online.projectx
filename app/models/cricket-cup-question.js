8bffc1384b62c5ee61b4140777d54bc660e3ee76
vdvibhu20 <vibhu.dujari@codingblocks.com>
2019-06-06 19:54:09 +0530

    incremental

--- app/models/cricket-cup-question.js
+++ app/models/cricket-cup-question.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
-  start: DS.attr(),
   description: DS.attr(),
   score: DS.attr(),
-  choices: DS.hasMany('cricketCupMatchChoices')
+  correctChoiceId: DS.attr(),
+  match: DS.belongsTo('cricketCupMatch')
 })
