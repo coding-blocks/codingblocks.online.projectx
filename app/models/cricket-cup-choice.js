fc2cd008fef9660ef3920f17e6f9640d43da6840
Abhishek Gupta <hi@itsabhishek.me>
2019-06-07 18:19:21 +0530

    haxor

--- app/models/cricket-cup-choice.js
+++ app/models/cricket-cup-choice.js
@@ -4,5 +4,8 @@ import env from 'codingblocks-online/config/environment'
 
 export default DS.Model.extend({
   content: DS.attr(),
+  title: computed('content', function () {
+    return this.get('content')
+  }),
   cricketCupQuestion: DS.belongsTo('cricketCupQuestion')
 })
