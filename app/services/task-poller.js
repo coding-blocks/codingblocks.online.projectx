6643dca7bc9116fa2d102da088cc13302236fce7
ananay <i@ananayarora.com>
2019-06-07 21:26:08 +0530

    Added docs to taskPoller service

--- app/services/task-poller.js
+++ app/services/task-poller.js
@@ -8,6 +8,14 @@ export default class TaskPollerService extends Service {
     this.performPoll = this.performPoll.bind(this)
   }
 
+  /**
+   * The actual ember concurrency task
+   * @param {function} fn – The function to execute
+   * @param {*} condition – Condition to check for upon which it stops polling
+   * @param {*} maxTries – The max amount of tries after which it stops polling
+   * @param {*} gap – Sets the gap between every request
+   * @returns {Promise<object>} result – Return the poller task result
+   */
   @task 
   *pollerTask(fn, condition, maxTries, gap) {
     while (maxTries--) {
@@ -19,6 +27,14 @@ export default class TaskPollerService extends Service {
     throw new Error('TIMEOUT')
   }
   
+  /**
+   * Queues the ember concurrency task
+   * @param {function} fn – The function to execute
+   * @param {*} condition – Condition to check for upon which it stops polling
+   * @param {*} maxTries – The max amount of tries after which it stops polling
+   * @param {*} gap – Sets the gap between every request
+   * @returns {Promise<object>} result – Return the poller task result
+   */
   performPoll(fn, condition, maxTries = 10, gap = 2000) {
     if (typeof condition !== 'function') {
       throw new Error('The Exit Condition to poll must be function! Got: ', typeof condition)
