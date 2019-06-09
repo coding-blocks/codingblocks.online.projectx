434b927ced56e61e3eced98b479523ce27177631
Abhishek Gupta <hi@itsabhishek.me>
2019-06-09 13:49:42 +0530

    cut: hls.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -36,7 +36,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('./bower_components/hls.js/dist/hls.light.min.js')
   app.import('node_modules/@coding-blocks/motley/dist/app.css')
   app.import('node_modules/showdown-katex-studdown/dist/showdown-katex.js')
 
