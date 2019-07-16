094306640baeab890d2fcc67587c87266633f935
Ansh Puvvada <43775333+crusader2000@users.noreply.github.com>
2019-07-16 11:33:06 +0530

    Updated router.js

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 import googlePageview from './mixins/google-pageview';
-
+import RouterScroll from 'ember-router-scroll';
 const Router = EmberRouter.extend(googlePageview, {
   location: config.locationType,
   rootURL: config.rootURL
