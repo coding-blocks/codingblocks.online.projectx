341dfdc0fe89dade04409e5df53ab603ebbe7c9f
Abhishek Gupta <hi@itsabhishek.me>
2019-08-12 13:42:40 +0530

    allow caching public /section/:id requests

--- app/adapters/section.js
+++ app/adapters/section.js
@@ -3,6 +3,6 @@ import ApplicationAdapter from './application'
 export default ApplicationAdapter.extend({
     urlForFindRecord (id, modelName, snapshot) {
         let baseUrl = this.buildURL(modelName, id, snapshot)
-        return `${baseUrl}/?include=contents&exclude=contents.*&sort=content.section_content.order`
+        return `${baseUrl}/?include=contents&exclude=contents.*&sort=content.section_content.order&public=true`
     }
 })
