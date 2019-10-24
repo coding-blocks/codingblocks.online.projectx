d77c08ea695a75d8c37886cc2075f0f40011ebbc
Abhishek Gupta <hi@itsabhishek.me>
2019-10-24 14:21:02 +0530

    keep people from hellointern on hellointern pages only

--- app/pods/components/course-card/component.js
+++ app/pods/components/course-card/component.js
@@ -1,8 +1,10 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { alias } from '@ember/object/computed';
 
 export default class CourseCardComponent extends Component {
   @service session
+  @service domain
 
   didInsertElement () {
     this._super(...arguments)
