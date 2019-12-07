3f0d4b2ea0b90ef59bd7534f38d75259553693ce
Jatin Katyal <jatin.katyal13@gmail.com>
2019-12-07 20:55:33 +0530

    progress bar and title in player header

--- app/pods/components/player/player-header/component.js
+++ app/pods/components/player/player-header/component.js
@@ -1,4 +1,26 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import DS from 'ember-data';
 
-export default Component.extend({
-});
+export default class PlayerHeader extends Component {
+  @service api
+  @service store
+  @service player
+
+  @computed('player.contentId')
+  get content() {
+    if (this.player.contentId) {
+      return this.store.peekRecord('content', this.player.contentId)
+    }
+  }
+
+  @computed('player.runAttemptId')
+  get progress() {
+    if (this.player.runAttemptId) {
+      return DS.PromiseObject.create({
+        promise: this.api.request(`run_attempts/${this.player.runAttemptId}/progress`)
+      })
+    }
+  }
+}
