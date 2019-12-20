af469bdd5c8e818f34e3f77ab77498b4bf951f49
Abhishek Gupta <hi@itsabhishek.me>
2019-12-21 00:23:56 +0530

    stash

---
+++ app/pods/spin/route.js
@@ -0,0 +1,5 @@
+import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'codingblocks-online/mixins/authenticated-route-mixin';
+
+export default Route.extend(AuthenticatedRouteMixin, {
+});
