c89a50299089027b2d516ed736a6ebbf272d615f
Jatin Katyal <jatin.katyal13@gmail.com>
2019-12-22 00:19:30 +0530

    pagination and winnings scroll

--- app/pods/components/spin/spin-winnings/component.js
+++ app/pods/components/spin/spin-winnings/component.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 
-export default Component.extend({
-});
+export default class SpinWinning extends Component {
+  carouselId = 'winning-carousel'
+  itemId = 'winning-0'
+
+  @action
+  scroll(direction) {
+    const element = document.getElementById(this.carouselId)
+    const item = document.getElementById(this.itemId)
+    const leftOffset = (direction === 'left' ? -1 : 1) * item.offsetWidth
+
+    element.scrollBy({ behavior: 'smooth', left: leftOffset })
+  }
+}
