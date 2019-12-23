52013fccf9da4af1d1d75e3c5d4fd5bfc967e844
Jatin Katyal <jatin.katyal13@gmail.com>
2019-12-23 20:45:36 +0530

    spin and win only for admins right now

--- app/pods/spin/route.js
+++ app/pods/spin/route.js
@@ -1,5 +1,13 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'codingblocks-online/mixins/authenticated-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  currentUser: service(),
+
+  beforeModel() {
+    if (this.currentUser.user.roleId !== 1) {
+      this.transitionTo('index')
+    }
+  }
 });
