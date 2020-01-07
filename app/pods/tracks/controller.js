f693389f0ea3ee093b2fad833b7c70d78ba53b36
Jatin Katyal <jatin.katyal13@gmail.com>
2020-01-07 13:34:35 +0530

    track filtering task

--- app/pods/tracks/controller.js
+++ app/pods/tracks/controller.js
@@ -1,18 +1,9 @@
 import Controller from '@ember/controller';
-import { restartableTask } from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
 
 export default class Tracks extends Controller {
   @service store
 
-  @restartableTask onSearchTask = function *(opts) {
-    const careerTrack = yield this.store.queryRecord('career-track', {
-      custom: {
-        ext: 'url',
-        url: 'recommend'
-      },
-      opts
-    })
-    this.transitionToRoute('tracks.id', careerTrack.slug)
-  }
+  queryParams = ['status', 'professionId']
+  status = 'student'
 }
