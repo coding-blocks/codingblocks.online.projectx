54f3ca6764420c24c930fbb0f9744f2c17ac5ac8
Abhishek Gupta <hi@itsabhishek.me>
2020-01-08 15:37:55 +0530

    make /app/ authenticated route

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,8 +1,9 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
 import { getSeoSchemaForAllCourses } from "codingblocks-online/utils/seo";
+import AuthenticatedRouteMixin from 'codingblocks-online/mixins/authenticated-route-mixin';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
   // Don't wait for any api request, everything is lazy loaded on the home page
   headData: service(),
   currentUser: service(),
