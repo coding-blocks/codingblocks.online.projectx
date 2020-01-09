4bf7388e31c631462c14606e2563f3fda974248c
Abhishek Gupta <hi@itsabhishek.me>
2020-01-09 13:57:31 +0530

    add public layout

--- app/pods/tracks/controller.js
+++ app/pods/tracks/controller.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default class Tracks extends Controller {
   @service store
+  @service session
 
   queryParams = ['status', 'professionId']
   status = 'student'
