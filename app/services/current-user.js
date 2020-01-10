c35d8aa0ab3f30f28df4f3e2d611da9edd42c005
Abhishek Gupta <hi@itsabhishek.me>
2020-01-10 15:17:45 +0530

    add cookie

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -18,9 +18,7 @@ export default Service.extend({
     if (currentUser && currentUser.id) {
       return Promise.resolve(currentUser);
     }
-    document.cookie = `auth-jwt=${this.get(
-      "session.data.authenticated.jwt"
-    )}; path=/`;
+    document.cookie = `auth-jwt=${this.get("session.data.authenticated.jwt")}; domain=${window.location.host}; path=/`;
 
     return this.store
       .queryRecord("user", { custom: { ext: "url", url: "me" } })
