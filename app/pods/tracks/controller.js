2ac5f7529c71ab314443fd1c01c193e49f839b8a
Abhishek Gupta <hi@itsabhishek.me>
2020-01-16 23:02:14 +0530

    add otp enroll and wormhole to outsideContainer

--- app/pods/tracks/controller.js
+++ app/pods/tracks/controller.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 export default class Tracks extends Controller {
   @service store
   @service session
+  @service layout
 
   queryParams = ['status', 'professionId']
   status = 'student'
