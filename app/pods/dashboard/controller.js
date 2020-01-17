414185376be3b1b5ec678d58c9d07bcd4f6aed35
Jatin Katyal <jatin.katyal13@gmail.com>
2020-01-17 18:46:34 +0530

    dashboard my courses empty screen

--- app/pods/dashboard/controller.js
+++ app/pods/dashboard/controller.js
@@ -15,15 +15,22 @@ export default class Dashboard extends Controller {
   wishlist
   @alias('fetchAppliedJobsTask.lastSuccessful.value')
   appliedJobs
+  @alias('fetchCoursesTask.lastSuccessful.value')
+  runs
 
   @computed('fetchAppliedJobsTask.last', 'appliedJobs')
   get noAppliedJobs() {
-    return this.fetchAppliedJobsTask.last && !(this.fetchAppliedJobsTask.isRunning || this.appliedJobs.length)
+    return this.fetchAppliedJobsTask.last && !this.appliedJobs.length
   }
 
   @computed('fetchWishlistCoursesTask.last', 'wishlist')
   get noWishlist() {
-    return this.fetchWishlistCoursesTask.last && !(this.fetchWishlistCoursesTask.isRunning || this.wishlist.length)
+    return this.fetchWishlistCoursesTask.last && !this.wishlist.length
+  }
+
+  @computed('fetchCoursesTask.last', 'runs')
+  get noRuns() {
+    return this.fetchCoursesTask.last && !this.runs.length
   }
 
   @restartableTask fetchPerformanceStatsTask = function *() {
--- app/pods/dashboard/route.js
+++ app/pods/dashboard/route.js
@@ -3,15 +3,15 @@ import AuthenticatedRouteMixin from 'codingblocks-online/mixins/authenticated-ro
 
 export default class Dashboard extends Route.extend(AuthenticatedRouteMixin) {
 
-  async model() {
-    return await this.store.queryRecord('run', {
+  model() {
+    return this.store.queryRecord('run', {
       custom: {
         ext: 'url',
         url: 'lastAccessedRun',
         
       },
       include: 'course,run_attempts'
-    })
+    }).catch(console.log)
   }
   setupController(controller, model) {
     controller.set('lastAccessedRun', model)
