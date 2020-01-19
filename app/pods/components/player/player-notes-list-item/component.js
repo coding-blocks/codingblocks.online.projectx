5daab910e1b67ce1a87a8b74856935275b7be213
Jatin Katyal <jatin.katyal13@gmail.com>
2020-01-19 16:50:59 +0530

    ask doubt and notes design fix

--- app/pods/components/player/player-notes-list-item/component.js
+++ app/pods/components/player/player-notes-list-item/component.js
@@ -1,10 +1,17 @@
 import Component from '@ember/component';
 import { restartableTask, dropTask } from 'ember-concurrency-decorators';
-import { action } from '@ember/object';
+import { action, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
+import moment from 'moment';
 
 export default class PlayerNotesListItem extends Component {
   @service api
+
+  @computed('note.duration')
+  get displayDuration() {
+    const duration = moment.duration(this.note.duration, 'seconds')
+    return `${duration.minutes()}:${duration.seconds()}`
+  }
 
   @restartableTask saveNotesTask = function *() {
     yield this.note.save()
