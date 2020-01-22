de247b5ea9f7359f0e9b9ac4e319ccd186c8ec13
Jatin Katyal <jatin.katyal13@gmail.com>
2020-01-22 17:59:32 +0530

    extension widget fixes

--- app/pods/components/extensions-widget/component.js
+++ app/pods/components/extensions-widget/component.js
@@ -4,21 +4,19 @@ import { inject as service } from '@ember/service';
 import env from "codingblocks-online/config/environment";
 
 export default class ExtensionsWidgetComponent extends Component {
-
   @service api
-
-  isVisible = false
 
   @restartableTask extensionsTask = function* ()  {
     const product = yield this.api.request('/runs/products/' + this.run.get('productId'))
     
-    this.set('isVisible', !!product.product_extensions.length)
-    return product.product_extensions.map(ext => ({
-      id: ext.id,
-      name: ext.name,
-      expiry: moment.unix(this.run.get('end')).add(ext.duration, "days").format("DD MMM YYYY"),
-      price: Math.ceil(ext.mrp/100)
-    }))
+    return product.product_extensions
+      .map(ext => ({
+        id: ext.id,
+        name: ext.name,
+        expiry: moment.unix(this.run.get('end')).add(ext.duration, "days").format("DD MMM YYYY"),
+        price: Math.ceil(ext.mrp/100)
+      }))
+      .sortBy('price')
   }
 
   @restartableTask buyExtensionTask = function *() {
@@ -28,10 +26,5 @@ export default class ExtensionsWidgetComponent extends Component {
     })
 
     window.location.href = env.dukaanUrl
-  }
-
-  didReceiveAttrs() {
-    this._super(...arguments)
-    this.extensionsTask.perform()
   }
 }
