1ce2c35648d9e2ea4e6bb6c07f111a8658222ebe
Jatin Katyal <jatin.katyal13@gmail.com>
2020-01-24 00:22:01 +0530

    doubts and notes are of current content only

--- app/pods/attempt/content/controller.js
+++ app/pods/attempt/content/controller.js
@@ -1,6 +1,62 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { restartableTask } from 'ember-concurrency-decorators';
 
 export default class AttemptController extends Controller {
   @service currentUser
+  @service player
+  @service store
+
+  tabs = [
+    {
+      name: 'Doubts',
+      component: 'player/player-doubts-tab',
+      task: this.fetchDoubtsTask
+    },
+    {
+      name: 'Notes',
+      component: 'player/player-notes-tab',
+      task: this.fetchNotesTask
+    }
+  ]
+  activeTab = this.tabs.firstObject
+  contentListCollpased = true
+
+  @restartableTask fetchDoubtsTask = function *() {
+    return this.store.query('doubt', {
+      filter: {
+        runAttemptId: this.player.runAttemptId,
+        contentId: this.player.contentId
+      }
+    })
+  }
+
+  @restartableTask fetchNotesTask = function *() {
+    return this.store.query('note', {
+      filter: {
+        runAttemptId: this.player.runAttemptId,
+        contentId: this.player.contentId
+      }
+    })
+  }
+
+  @action 
+  openAskDoubtModal() {
+    const content = this.store.peekRecord('content', this.player.contentId)
+    const runAttempt = this.store.peekRecord('run-attempt', this.player.runAttemptId)
+    const doubt = this.store.createRecord('doubt', {
+      content,
+      runAttempt
+    })
+
+    this.set('newDoubt', doubt)
+    this.set('showAskDoubtModal', true)
+  }
+
+  @action
+  closeAskDoubtModal() {
+    this.newDoubt.rollbackAttributes()
+    this.set('showAskDoubtModal', false)
+  }
 }
