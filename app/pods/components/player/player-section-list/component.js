d8d18ca8b6627a073149dec769ea1b2ba8911bbd
Abhishek Gupta <hi@itsabhishek.me>
2020-01-23 18:14:43 +0530

    minor ui fixes

--- app/pods/components/player/player-section-list/component.js
+++ app/pods/components/player/player-section-list/component.js
@@ -1,9 +1,5 @@
 import Component from '@ember/component';
 
 export default class SectionList extends Component {
-  didReceiveAttrs() {
-    if (!this.initialSectionId) {
-      this.set('initialSectionId', this.currentSectionId)
-    }
-  }
+
 }
