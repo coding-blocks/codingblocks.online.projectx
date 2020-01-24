a39e07be31ff8fe1c90308fed220ba95596ca744
Jatin Katyal <jatin.katyal13@gmail.com>
2020-01-24 18:15:10 +0530

    remove jank in track.courses

--- app/pods/tracks/id/controller.js
+++ app/pods/tracks/id/controller.js
@@ -1,9 +1,15 @@
 import Controller from '@ember/controller';
 import { restartableTask } from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default class Track extends Controller {
   @service store
+
+  @computed('track.courses')
+  get coursesLoaded() {
+    return this.track.hasMany('courses').value()
+  }
 
   @restartableTask fetchInstructorsTask = function *() {
     const instructors = yield this.store.query('instructor', {
