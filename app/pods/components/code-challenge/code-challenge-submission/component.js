cb9fe7d8ef64f96e113dc4ca73421d4fc678425c
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-03 15:14:14 +0530

    view submission in submissions tab

--- app/pods/components/code-challenge/code-challenge-submission/component.js
+++ app/pods/components/code-challenge/code-challenge-submission/component.js
@@ -1,3 +1,13 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
 
-export default class SubmissionComponent extends Component {}
+export default class SubmissionComponent extends Component {
+  submissionToView = null
+  viewSubmissionCode = false
+
+  @action
+  viewSubmission(submission) {
+    this.set('submissionToView', submission)
+    this.set('viewSubmissionCode', true)
+  }
+}
