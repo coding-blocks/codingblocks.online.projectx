314f30dc7444b936c93880fed6c1a89a9e56dc27
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-06 19:50:07 +0530

    webinar timer added

--- app/pods/attempt/content/webinar/controller.js
+++ app/pods/attempt/content/webinar/controller.js
@@ -1,10 +1,26 @@
 import Controller from '@ember/controller';
 import { dropTask } from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default class WebinarController extends Controller {
   @service api
   @service player
+
+  started = false
+
+  @computed('webinar.{start_time,duration}', 'started')
+  get webinarStatus() {
+    const start_time = moment(this.webinar.get('start_time'))
+    const end_time = moment(start_time).add(this.webinar.get('duration'), 'minutes')
+    // will start
+    if (start_time.isAfter() && !this.started) return -1
+    // live
+    if (start_time.isBefore() && end_time.isAfter()) return 0
+    // ended
+    if (end_time.isBefore()) return 1
+  }
 
   @dropTask startWebinarTask = function *() {
     const response = yield this.api.request(`webinars/${this.get('webinar.id')}/register`, {
