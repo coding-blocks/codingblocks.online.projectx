3fd551bfaabef0a45a74e5acc69c33b417fa8fdc
Abhishek Gupta <hi@itsabhishek.me>
2020-02-09 14:27:07 +0530

    fix: progress not updating

--- app/utils/content.js
+++ app/utils/content.js
@@ -2,7 +2,6 @@ export const defaultProgressValueForContent = content => {
   switch (content.contentable) {
     case 'code-challenge':
     case 'lecture':
-    case 'video':
       return 'ACTIVE'
     default:
       return 'DONE'
