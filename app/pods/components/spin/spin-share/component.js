cd0eacb3ab25ced166fc13e25ea40f1735a55286
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-11 13:52:35 +0530

    share component

---
+++ app/pods/components/spin/spin-share/component.js
@@ -0,0 +1,37 @@
+import Component from '@ember/component';
+import { action, computed } from '@ember/object';
+
+export default class SpinShare extends Component {
+  linksMap = {
+    'whatsapp': text => `https://web.whatsapp.com/send?text=${text}`,
+    'twitter': text => `http://twitter.com/share?text=${text}&url=https://online.codingblocks.com&hashtags=codingBlocksIN`,
+    'facebook': text => `https://www.facebook.com/sharer/sharer.php?u=online.codingblocks.com&quote=${text}`
+  }
+
+  @computed('referralCode')
+  get shareText() {
+    return `Sign-up using my link to get instant 500 in your wallet and Spin the CB Wheel to win assured prizes this Christmas and New Year using my referral link: https://cb.lk/join/${this.referralCode.code}  @codingblocksIn
+
+    #CodingBlocks #CBSanta #Christmas #NewYear`
+  }
+
+  @action
+  selectText(containerid) {
+    if (document.selection) { // IE
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+    }
+    document.execCommand('copy');
+  }
+
+  @action
+  share(to) {
+    window.open(this.linksMap[to](this.shareText), `${to}-share`, 'width=860,height=840,toolbar=0,menubar=0')
+  }
+}
--- app/pods/spin/index/controller.js
+++ app/pods/spin/index/controller.js
@@ -15,19 +15,6 @@ export default class SpinIndexController extends Controller {
   showTnC = false
   prizeDrawn = null
 
-  linksMap = {
-    'whatsapp': text => `https://web.whatsapp.com/send?text=${text}`,
-    'twitter': text => `http://twitter.com/share?text=${text}&url=https://online.codingblocks.com&hashtags=codingBlocksIN`,
-    'facebook': text => `https://www.facebook.com/sharer/sharer.php?u=online.codingblocks.com&quote=${text}`
-  }
-
-  @computed('referralCode')
-  get shareText() {
-    return `Sign-up using my link to get instant 500 in your wallet and Spin the CB Wheel to win assured prizes this Christmas and New Year using my referral link: https://cb.lk/join/${this.referralCode.code}  @codingblocksIn
-
-    #CodingBlocks #CBSanta #Christmas #NewYear`
-  }
-
   @alias('spin.isRunning')
   isSpinning
     
@@ -94,25 +81,5 @@ export default class SpinIndexController extends Controller {
 
     this.set('notVerifiedEmailModal', false)
     return ;
-  }
-
-  @action
-  share(to) {
-    window.open(this.linksMap[to](this.shareText), `${to}-share`, 'width=860,height=840,toolbar=0,menubar=0')
-  }
-
-  @action
-  selectText(containerid) {
-    if (document.selection) { // IE
-        var range = document.body.createTextRange();
-        range.moveToElementText(document.getElementById(containerid));
-        range.select();
-    } else if (window.getSelection) {
-        var range = document.createRange();
-        range.selectNode(document.getElementById(containerid));
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(range);
-    }
-    document.execCommand('copy');
   }
 }
