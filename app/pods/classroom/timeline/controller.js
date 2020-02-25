01bde1dcc224fd15208b8ee3b480d411db31a307
Abhishek Gupta <hi@itsabhishek.me>
2020-02-25 17:28:46 +0530

    show error prompt if runAttempt is revoked

--- app/pods/classroom/timeline/controller.js
+++ app/pods/classroom/timeline/controller.js
@@ -1,3 +1,7 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
-export default class TimelineController extends Controller {}
+export default class TimelineController extends Controller {
+  @service currentUser
+
+}
