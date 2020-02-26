9be3488575533b18c3b381097dee3d93c94cfcad
Abhishek Gupta <hi@itsabhishek.me>
2020-02-26 15:19:45 +0530

    show login blocker screen

--- app/pods/login-blocker/controller.js
+++ app/pods/login-blocker/controller.js
@@ -3,19 +3,28 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class LoginBlockerController extends Controller {
+  @service api
+  @service session
+  @service currentUser
+  
   queryParams = ['token']
-
   token = null
 
-  @service api
 
   @action
   async logoutFromAll() {
-    await this.api.request('/jwt/logoutAll', {
+    const response = await this.api.request('/jwt/logoutAll', {
       headers: {
         'Authorization': `JWT ${this.token}`
       }
     })
-    window.location.href = '/'
+    await this.session.authenticate('authenticator:jwt-direct', response)
+    await this.currentUser.load()
+
+    if(user.get('organization')) {
+      this.transitionTo(user.get('organization'))
+    }
+
+    // window.location.href = '/app/'
   }
 }
