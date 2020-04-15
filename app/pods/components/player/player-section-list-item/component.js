996090ac125c25c3ed426328b773741ce2b1f271
Ashvary <ashvaryvirmani@gmail.com>
2020-04-15 21:01:24 +0530

    Don't show Free tag for users who have premium enrollment and is not expired

--- app/pods/components/player/player-section-list-item/component.js
+++ app/pods/components/player/player-section-list-item/component.js
@@ -6,15 +6,24 @@ import { computed } from '@ember/object';
 
 export default class Selection extends Component {
   @service player
-
   @alias('player.contentId') activeContentId
   @alias('player.sectionId') activeSectionId
-
+  @service store;
   @computed('activeSectionId', 'section.id')
   get isActiveSection() {
+
     return this.activeSectionId == this.section.id
   }
-  
+
+  @computed('player.runAttemptId')
+  get runAttemptPremium() {
+    if (this.player.runAttemptId) {
+      const run_attempt = this.store.peekRecord('run-attempt', this.player.runAttemptId)
+      const not_expired = run_attempt.end > Date.parse(moment())/1000
+      return run_attempt.premium && not_expired
+    }
+  }
+
   @action
   toggle() {
     this.toggleProperty('isOpen')
