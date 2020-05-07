018a6dd44333953ac91480afedcc352e4b5a63b1
1arp <dekhijaegi@gmail.com>
2020-05-07 20:07:18 +0530

    redirect nonNoagarro to /courses from /app/courses

--- app/pods/courses/route.js
+++ app/pods/courses/route.js
@@ -1,5 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
 
 export default class CoursesRouter extends Route {
-    
+  @service currentUser
+  activate(){
+    if(!this.currentUser.organistaion){
+      window.location.href='/courses'
+    }
+  }
 }
