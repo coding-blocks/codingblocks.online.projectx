e09edc4ee2ff5461d17a43b54e04195be526c3cd
Abhishek Gupta <hi@itsabhishek.me>
2020-05-11 16:04:35 +0530

    send cookies along with api req

--- app/services/api.js
+++ app/services/api.js
@@ -1,22 +1,26 @@
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
-import env from 'codingblocks-online/config/environment';
-import AjaxService from 'ember-ajax/services/ajax';
+import { inject as service } from "@ember/service";
+import { computed } from "@ember/object";
+import env from "codingblocks-online/config/environment";
+import AjaxService from "ember-ajax/services/ajax";
 
 export default AjaxService.extend({
-    session: service(),
-    host: env.apiHost,
-    contentType: 'application/json; charset=utf-8',
-    namespace: '/api/v2',
-    headers: computed ('session.data.authenticated.jwt', function () {
-        let headers = {};
-        const jwt = this.get('session.data.authenticated.jwt');
-        if (jwt) {
-            headers['Authorization'] = `JWT ${jwt}`;
-          }
-        return headers;
-    }),
-    xhrFields: {
-      withCredentials: true
-    },
+  session: service(),
+  host: env.apiHost,
+  contentType: "application/json; charset=utf-8",
+  namespace: "/api/v2",
+  headers: computed("session.data.authenticated.jwt", function () {
+    let headers = {};
+    const jwt = this.get("session.data.authenticated.jwt");
+    if (jwt) {
+      headers["Authorization"] = `JWT ${jwt}`;
+    }
+    return headers;
+  }),
+  options() {
+    const res = this._super(...arguments)
+    res.xhrFields = {
+      withCredentials: true,
+    }
+    return res
+  }
 });
