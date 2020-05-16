7ded2f102c4739543d88f8d0c0366c460b005b4b
1arp <dekhijaegi@gmail.com>
2020-05-17 00:27:10 +0530

    pasue changes on dashboard

---
+++ app/pods/components/dashboard/dashboard-courses-list-item/component.js
@@ -0,0 +1,15 @@
+import Component from '@ember/component';
+import { action, computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class DashBoardCoursesListItem  extends Component {
+  @service api
+  @action
+  async unpauseRunAttempt() {
+    await this.get('api').request(`run_attempts/${this.run.topRunAttempt.id}/unpause`, {
+      method: 'PATCH'
+    })
+    return this.set('run.topRunAttempt.paused', false)
+  }
+
+};
