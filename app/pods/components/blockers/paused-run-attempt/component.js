d61ec1f82fa002ec79d8ba869cd67c3b455d0dde
1arp <dekhijaegi@gmail.com>
2020-05-17 12:34:44 +0530

    removed imports not being used

--- app/pods/components/blockers/paused-run-attempt/component.js
+++ app/pods/components/blockers/paused-run-attempt/component.js
@@ -1,15 +1,14 @@
 import Component from '@ember/component';
-import { action, computed } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service'
 
-export default Component.extend({
-  api : service(),
-  actions:{
-    async unpauseRunAttempt() {
-      await this.get('api').request(`run_attempts/${this.runAttempt.id}/unpause`, {
-        method: 'PATCH'
-      })
-      return this.runAttempt.reload()
-    }
-  },
-});
+export default class PausedRunAttempt  extends Component {
+  @service api
+  @action
+  async unpauseRunAttempt() {
+    await this.get('api').request(`run_attempts/${this.runAttempt.id}/unpause`, {
+      method: 'PATCH'
+    })
+    return this.runAttempt.reload()
+  }
+}
--- app/pods/components/dashboard/dashboard-courses-list-item/component.js
+++ app/pods/components/dashboard/dashboard-courses-list-item/component.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { action, computed } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class DashBoardCoursesListItem  extends Component {
