a04d9384f635ff3fa10bddb801897b701e0ecab1
Ashvary <ashvaryvirmani@gmail.com>
2020-05-25 18:09:02 +0530

    Certificate Blocker Modal Created

---
+++ app/pods/components/blockers/certificate/component.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+import {action} from '@ember/object'
+import { inject as service } from '@ember/service';
+
+export default class BlockersCertificateComponent extends Component {
+  @service api
+  @service store
+  @service router
+  
+}
