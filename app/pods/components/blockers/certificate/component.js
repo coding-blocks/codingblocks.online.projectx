a373a5c41eb1c5e8913690c101fede8c8d1a8a1f
Abhishek Gupta <hi@itsabhishek.me>
2020-05-27 15:59:14 +0530

    cleanup

--- app/pods/components/blockers/certificate/component.js
+++ app/pods/components/blockers/certificate/component.js
@@ -1,15 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class BlockersCertificateComponent extends Component {
-  @service api
-  @service store
-  @service router
   @service currentUser
- 
-  @action
-  continue() {
-    this.get('onContinue')()
-  }
 }
