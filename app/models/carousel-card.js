1d02422073251b5690768437ff296dc1f3acfffd
BipinKalra <bipinkalra@gmail.com>
2020-07-12 00:58:04 +0530

    purple gradient changes

--- app/models/carousel-card.js
+++ app/models/carousel-card.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   title: DS.attr("string"),
   subtitle: DS.attr("string"),
   img: DS.attr("string"),
-  theme: DS.attr('string', { defaultValue: 'bg-gradient-purple' }),
+  theme: DS.attr('string', { defaultValue: 'bg-gradient-dpurple' }),
   buttonLink: DS.attr("string"),
   buttonText: DS.attr("string"),
   order: DS.attr("number")
