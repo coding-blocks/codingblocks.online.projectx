f9ea7bb6a82df4849b4c47ff8c6b4353fa3b54d0
abhishek97 <hi@itsabhishek.me>
2020-09-24 05:13:48 +0530

    derp

--- app/pods/components/content-feedback/component.js
+++ app/pods/components/content-feedback/component.js
@@ -1,17 +1,56 @@
 import Component from '@ember/component';
+import { action } from '@ember/object';
+import { computed } from '@ember/object';
 
-export default Component.extend({
-    feedbackState: 'COLLAPSED',
-    actions: {
-        changeState(newState) {
-            this.set('feedbackState', newState)
-        },
-        submitFeedback(feed) {
-            this.onFeedbackSubmit(feed)
-            this.send('changeState', 'SUBMITTED')
-            setTimeout(() => {
-                this.set('showFeedback', false)
-            }, 2000)
-        }
-    }
-});
+const BAD_REASONS = [
+	'Could not understand topic.',
+	'Audio was not clear.',
+	'Concept was covered too fast.',
+	'Concept was covered too slow.',
+	'Prerequisites were not covered.'
+]
+
+const GOOD_REASONS = [
+	'Concept was convered is best possible manner'
+]
+
+export default class ContentFeedbackComponent extends Component {
+	expanded = false
+	selectedRating = null
+
+	@action
+	toggleExpandedView() {
+		// this.expanded && this.set('selectedRating', null)
+		this.toggleProperty('expanded')
+	}
+
+	@computed('selectedRating')
+	get expansionType () {
+		const rating = this.selectedRating
+		if (rating === 4 || rating === null)
+			return 'NONE'
+		else if (rating <= 3) 
+			return 'BAD'
+		 else
+			return 'GOOD'
+	}
+
+	@computed('expansionType')
+	get expansionText () {
+		switch(this.expansionType) {
+			case 'BAD': return 'What went wrong?';
+			case 'GOOD': return 'What went awesome?';
+			default: return ''
+		}
+	}
+
+	@computed('expansionType')
+	get expansionReasons() {
+		switch(this.expansionType) {
+			case 'BAD': return BAD_REASONS;
+			case 'GOOD': return GOOD_REASONS;
+			default: return []
+		}
+	}
+
+}
