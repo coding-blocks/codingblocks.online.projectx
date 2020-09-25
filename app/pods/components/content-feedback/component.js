2fe955f400dae71c6f211c537d94eb630b0d35b8
abhishek97 <hi@itsabhishek.me>
2020-09-25 23:25:26 +0530

    add thanks view

--- app/pods/components/content-feedback/component.js
+++ app/pods/components/content-feedback/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import { action } from '@ember/object';
 import { computed } from '@ember/object';
+import { restartableTask } from 'ember-concurrency-decorators';
 
 const BAD_REASONS = [
 	'Could not understand topic.',
@@ -11,22 +12,28 @@ const BAD_REASONS = [
 ]
 
 const GOOD_REASONS = [
-	'Concept was convered is best possible manner'
+	'Great examples used',
+	'Simple Explanation for Complex Concept',
+	'Flawless Audio/Visual experience',
+	'Useful code snippets shared '
 ]
 
 export default class ContentFeedbackComponent extends Component {
 	expanded = false
 	selectedRating = null
+	selectedReason = null
+	sayThankYou = false
 
 	didReceiveAttrs () {
 		this._super(...arguments)
-		this.selectedRating = this.progress
+		this.selectedRating = this.progress.get('feedback.rating')
+		this.selectedReason = this.progress.get('feedback.reason')
 	}	
 
 	@computed('selectedRating')
 	get expansionType () {
 		const rating = this.selectedRating
-		if (rating === 4 || rating === null)
+		if (rating === 4 || !rating)
 		return 'NONE'
 		else if (rating <= 3) 
 		return 'BAD'
@@ -51,21 +58,35 @@ export default class ContentFeedbackComponent extends Component {
 			default: return []
 		}
 	}
-	
-	@action
-	async updateContentFeedback (reason = '') {
-		// update everything in progress
+
+	@restartableTask updateContentFeedbackTask = function *(reason = '') {
 		const feedback = {
 			rating: this.selectedRating,
 			reason
 		}
 		this.progress.set('feedback', feedback)
-		await this.progress.save()
+		yield this.progress.save()
+		this.set('sayThankYou', true)
 	}
 	
 	@action
 	toggleExpandedView() {
 		// this.expanded && this.set('selectedRating', null)
 		this.toggleProperty('expanded')
+	}
+
+	@action
+	setRating (value) {
+		if (value === 4) {
+			// short circuit flow -> dont wait for reasons; just submit
+			return this.updateContentFeedbackTask.perform()
+		}
+
+		this.set('selectedRating', value)
+	}
+
+	@action
+	close () {
+		this.set('expanded', false)
 	}
 }
