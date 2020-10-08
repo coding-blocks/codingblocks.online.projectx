51a6effb398993998605b461f6fe905d5d30e8fb
abhishek97 <hi@itsabhishek.me>
2020-10-08 18:39:44 +0530

    cleanup: application.route::beforeModel

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -24,45 +24,54 @@ export default Route.extend(ApplicationRouteMixin, UtmCookieRouteMixin, {
     this._super(...arguments)
     this.metrics; // !important: keep this here to init trackers for all routes
     this.onesignal;
-    if (!isNone(transition.to.queryParams.code)) {
-      if (this.get("session.isAuthenticated")) {
-        return "";
-        // return this.transitionTo({ queryParams: { code: undefined } })
-      }
-      // we have ?code qp
-      const { code } = transition.to.queryParams;
 
-      return this.session
-        .authenticate("authenticator:jwt", {
-          identification: code,
-          password: code,
-          code
-        })
-        .then(() => this.currentUser.load())
-        .then(user => {
-          // if user belongs to an org, redirect to the domain
-          if (user.get("organization")) {
-            this.transitionTo(user.get("organization"));
-          }
-        })
-        .catch(error => {
-          if (error.err === "USER_EMAIL_NOT_VERIFIED") {
-            return this.transitionTo("error", {
-              queryParams: {
-                errorCode: "USER_EMAIL_NOT_VERIFIED"
-              }
-            });
-          }
-          if (error.name == "USER_LOGGED_IN_ELSEWHERE") {
-            return this.transitionTo("login-blocker", {
-              queryParams: {
-                code: null,
-                token: error.logout_token
-              }
-            });
+    if (isNone(transition.to.queryParams.code))  // do nothing if no code query param is present
+      return ;
+
+    
+    if (this.get("session.isAuthenticated")) { // do nothing if user already authenticated
+      return ;
+      // return this.transitionTo({ queryParams: { code: undefined } })
+    }
+
+    // we have ?code qp
+    const { code } = transition.to.queryParams;
+
+    try {
+      // try logging in using the grant code in qp
+      await this.session.authenticate("authenticator:jwt", {
+        identification: code,
+        password: code,
+        code
+      })
+
+      // if login is successfull; load the current user
+      const user = await this.currentUser.load()
+      
+      // if this user is from some org; redirect to the org page
+      if (user.get("organization")) {
+        this.transitionTo(user.get("organization"));
+      }
+
+    } catch (error) {
+      // handle some known errors
+      if (error.err === "USER_EMAIL_NOT_VERIFIED") {
+        return this.transitionTo("error", {
+          queryParams: {
+            errorCode: "USER_EMAIL_NOT_VERIFIED"
           }
         });
+      }
+      if (error.name == "USER_LOGGED_IN_ELSEWHERE") {
+        return this.transitionTo("login-blocker", {
+          queryParams: {
+            code: null,
+            token: error.logout_token
+          }
+        });
+      }
     }
+
   },
   model() {
     return this.get("session.isAuthenticated") && this.currentUser.load()
