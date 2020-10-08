b0eae97c0427a99725eb0c65a4906b15fbac5419
abhishek97 <hi@itsabhishek.me>
2020-10-08 18:39:57 +0530

    fix redirectionPath resolution

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,11 +1,26 @@
 import Route from "@ember/routing/route";
 import { later } from '@ember/runloop';
 
+const ABSOLUTE_PATH_PREFIX = "absolute_path:"
+
+/*
+  redirectTarget: absolute_path:http://online.codingblocks.com/courses   // set by Nuxt (must be absolute path as these can't be resolved inside ember)
+  redirectTarget: /app/classroom/  // can be resolved in ember, no need to reload entire page
+*/
+function handleRedirectionTarget (redirectTarget) {
+  const isAbsolutePath = redirectTarget.startsWith(ABSOLUTE_PATH_PREFIX)
+  if (isAbsolutePath) {
+    window.location.href = redirectTarget.replace(ABSOLUTE_PATH_PREFIX, "")
+  } else {
+    later(() => { this.transitionTo(redirectTarget, { code: null }) })
+  }
+}
+
 export default Route.extend({
   model() {
     const redirectTarget = window.localStorage.getItem('redirectionPath')
     if (redirectTarget && redirectTarget != '/') {
-      later(() => { this.transitionTo(redirectTarget, { code: null }) })
+      handleRedirectionTarget.call(this, redirectTarget)
       window.localStorage.removeItem('redirectionPath')
     } else {
       this.transitionTo('dashboard')
